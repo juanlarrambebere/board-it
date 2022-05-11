51a727b7ead5f6070dfc6514cb656f09e51006ae
juanlarrambebere <juan@streaver.com>
2022-05-11 17:21:40 -0300

    Configure ApolloProvider so it uses the backend client

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import { backendCLient } from "@graphql/apollo";
+import type { AppProps } from "next/app";
+import { FC } from "react";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ApolloProvider client={backendCLient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
