cdd6a9d67da5fc9228f6d45b3159ff996a15fcf2
juanlarrambebere <juan@streaver.com>
2022-05-12 20:10:55 -0300

    Removed unnecesary paths + Added a RecoilRoot in the _app component

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { ApolloProvider } from "@apollo/client";
-import { backendCLient } from "@graphql/apollo";
-import type { AppProps } from "next/app";
-import { FC } from "react";
-import "../styles/globals.css";
+import { ApolloProvider } from '@apollo/client';
+import { backendCLient } from 'graphql/apollo';
+import type { AppProps } from 'next/app';
+import { FC } from 'react';
+import { RecoilRoot } from 'recoil';
+import '../styles/globals.css';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <ApolloProvider client={backendCLient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <RecoilRoot>
+      <ApolloProvider client={backendCLient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </RecoilRoot>
   );
 };
 
