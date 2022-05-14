001c603eb1ae070660d079867a9de2530f8a3c3e
juanlarrambebere <juan@streaver.com>
2022-05-14 20:17:29 -0300

    New hook to update a task

---
+++ frontend/graphql/mutations/UpdateTask.ts
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+import { Raw } from 'types';
+
+export interface UpdateTaskData {
+  task: Raw.Task;
+}
+
+export interface UpdateTaskVariables {
+  taskId: number;
+  changes: Partial<Pick<Raw.Task, 'name' | 'description' | 'status'>>;
+}
+
+export const UPDATE_TASK = gql`
+  mutation UpdateTask($taskId: Int!, $changes: tasks_set_input!) {
+    task: update_tasks_by_pk(pk_columns: { id: $taskId }, _set: $changes) {
+      id
+    }
+  }
+`;
---
+++ frontend/hooks/useUpdateTask.ts
@@ -0,0 +1,56 @@
+import { useMutation } from '@apollo/client';
+import { parseJSON } from 'date-fns';
+import {
+  UpdateTaskData,
+  UpdateTaskVariables,
+  UPDATE_TASK,
+} from 'graphql/mutations/UpdateTask';
+import { useRecoilCallback } from 'recoil';
+import taskAtomFamily from 'recoil/atoms/taskAtomFamily';
+import { Raw } from 'types';
+import { Status } from 'types/status';
+
+const useUpdateTask = () => {
+  const [mutation] = useMutation<UpdateTaskData, UpdateTaskVariables>(
+    UPDATE_TASK
+  );
+
+  const updateTask = useRecoilCallback(
+    ({ set }) =>
+      async (
+        taskId: number,
+        changes: Partial<Pick<Raw.Task, 'name' | 'description' | 'status'>>
+      ) => {
+        const { data, errors } = await mutation({
+          variables: {
+            taskId,
+            changes,
+          },
+        });
+
+        if (!data || errors) {
+          // TODO log the error somewhere (ie: Sentry)
+          throw 'Error creating task';
+        }
+
+        // Since the mutation worked, its safe to update the state without waiting for the subscription.
+        const updatedTask = data.task!;
+        set(taskAtomFamily(taskId), (task) => ({
+          ...task,
+          ...{
+            id: updatedTask.id,
+            name: updatedTask.name,
+            description: updatedTask.description,
+            status: updatedTask.status as Status,
+            updatedAt: parseJSON(updatedTask.updated_at),
+            createdAt: parseJSON(updatedTask.created_at),
+          },
+        }));
+      },
+    [mutation]
+  );
+
+  return updateTask;
+};
+
+export default useUpdateTask;
