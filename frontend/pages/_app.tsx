6dd57e6e7dae9f61158233948f775245fda11b0c
juanlarrambebere <juan@streaver.com>
2022-05-15 22:24:46 -0300

    Install react-toastify to show notifications..

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,8 @@ import { ApolloProvider } from '@apollo/client';
 import { backendCLient } from 'graphql/apollo';
 import type { AppProps } from 'next/app';
 import { FC } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
 
@@ -9,6 +11,17 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <RecoilRoot>
       <ApolloProvider client={backendCLient}>
+        <ToastContainer
+          position="top-right"
+          autoClose={3500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover
+        />
         <Component {...pageProps} />
       </ApolloProvider>
     </RecoilRoot>
