17498884a92d2ad6718a9e5f9e43e1b506ad6769
juanlarrambebere <juan@streaver.com>
2022-05-15 23:09:14 -0300

    Added a heading to the page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,16 @@
 import KanbanBoard from 'components/KanbanBoard';
 import useSubscribeToTasks from 'hooks/useSubscribeToTasks';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   useSubscribeToTasks();
 
   return (
     <>
+      <Head>
+        <title>Board it</title>
+      </Head>
       <div className="flex justify-center h-full min-h-screen text-neutral-300 bg-neutral-800">
         <KanbanBoard />
       </div>
